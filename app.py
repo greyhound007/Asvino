2219f0b295dc1367df2c2c4a7cf44ea91ca1f55e
EltonRayan <45653117+EltonRayan@users.noreply.github.com>
2020-04-13 22:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 #UPLOAD_FOLDER = os.path.join(PARENT_DIR, 'gene_data')
 app = Flask(__name__)
 app.secret_key = "secret key"
-#
-'''app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
-client = bigquery.Client.from_service_account_json("C:\Users/rayel\Downloads\secure-potion-270613-c923fa84a6fc.json")
-dataset_id = "{}.risk_analysis".format(client.project)
-'''#
+
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
+#client = bigquery.Client.from_service_account_json("C:\Users/rayel\Downloads\secure-potion-270613-c923fa84a6fc.json")
+#dataset_id = "{}.risk_analysis".format(client.project)
+
