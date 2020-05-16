f79a1280f2c785a0b1e579ab05ce2463b02d7591
EltonRayan <45653117+EltonRayan@users.noreply.github.com>
2020-05-16 15:35:40 +0530

    3rd update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 
 
 
-#UPLOAD_FOLDER = os.path.join(PARENT_DIR, 'gene_data')
+
 app = Flask(__name__)
 app.secret_key = "secret key"
+client = bigquery.Client.from_service_account_json("C:\Users/rayel\Downloads\secure-potion-270613-c923fa84a6fc.json")
+dataset_id = "{}.risk_analysis".format(client.project)
