ad7c9457c4557d9cae096bacf66a17777a739903
EltonRayan <45653117+EltonRayan@users.noreply.github.com>
2020-05-16 17:13:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-client = bigquery.Client.from_service_account_json("C:\Users/rayel\Downloads\secure-potion-270613-c923fa84a6fc.json")
+client = bigquery.Client()
 dataset_id = "{}.risk_analysis".format(client.project)
